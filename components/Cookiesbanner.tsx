f7d83d6a5cc890c2b97c3b298b40059f2ebc5009
dea <108593938+deaamanger@users.noreply.github.com>
2023-07-29 16:53:17 +0200

    v5.2 Cookies style update

--- components/Cookiesbanner.tsx
+++ components/Cookiesbanner.tsx
@@ -27,32 +27,37 @@ export default function CookieBanner() {
   }, [cookieConsent]);
   return (
     <div
-      className={`my-10 z-50 mx-auto max-w-max md:max-w-screen-sm
-                        fixed bottom-0 left-0 right-0 
-                      ${
-                        cookieConsent != null ? "hidden" : "flex"
-                      }     flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
-                         bg-gray-700 rounded-lg shadow`}
+      className={`my-10 z-50 mx-auto max-w-max md:max-w-screen-md
+      ${cookieConsent != null ? "hidden" : "flex"}
+                        fixed bottom-24 left-0 right-0 h-[45%]
+                          flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
+                         bg-[#111]/90 rounded-lg shadow`}
     >
-      <div className="text-center">
-        <Link href="/info/cookies">
-          <p>
-            We use <span className="font-bold text-sky-400">cookies</span> on
-            our site.
-          </p>
-        </Link>
+      <div className="mt-10 ">
+        <p className=" flex flex-col items-start justify-center space-y-8 h-full w-full">
+          <span className="text-amber-500 pl-10 text-2xl animate-pulse ">
+            Cookies for Analysis :
+          </span>
+          <span className="text-gray-300 px-2 md:px-5">
+            I use cookies to analyze website traffic and improve your browsing
+            experience. By continuing to use this website, you consent to the
+            use of cookies for analytical purposes. For more information, please
+            read our{" "}
+            <span className="text-blue-300 underline">Privacy Policy</span> .
+          </span>
+        </p>
       </div>
 
-      <div className="flex gap-2">
+      <div className="flex gap-2 mb-8 md:mb-0">
         <button
-          onClick={() => setCookieConsent(false)}
-          className="px-5 py-2 text-gray-300 rounded-md border-gray-900"
+          onClick={() => setCookieConsent(true)}
+          className="px-5 py-2 text-gray-300 rounded-lg bg-gray-900   border-gray-200"
         >
           Decline
         </button>
         <button
           onClick={() => setCookieConsent(true)}
-          className="bg-gray-900 px-5 py-2 text-white rounded-lg"
+          className="bg-gray-900 px-5 py-2 text-white rounded-lg "
         >
           Allow Cookies
         </button>
