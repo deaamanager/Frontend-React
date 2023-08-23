8a4ca28663bf5dedbf15e4f07b1670554ecee779
dea <108593938+deaamanger@users.noreply.github.com>
2023-08-23 20:40:14 +0200

    thankyou Page v1.1

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -1,15 +1,13 @@
 "use client";
 
-import Chatbott from "@/components/Chatbot";
 import ThankyouPage from "@/components/ThankyouPage";
 
 function page() {
   return (
     <>
-      <div className="h-screen max-w-7xl mx-auto flex-col flex  items-center justify-start pt-24  gap-y-8 ">
+      <div className="h-screen w-full flex-col flex  items-center justify-start pt-24  gap-y-8 ">
         <ThankyouPage />
       </div>
-      <Chatbott />
     </>
   );
 }
--- components/ThankyouPage.tsx
+++ components/ThankyouPage.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import Chatbott from "./Chatbot";
 
 function ThankyouPage() {
   return (
