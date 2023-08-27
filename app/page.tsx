1d5c71089d51b986fd46e83d1bbdcaea5623e5a6
Deaa Aldin Alawad <108593938+deaamanger@users.noreply.github.com>
2023-08-27 15:55:46 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import { Suspense } from "react";
 export default function Home() {
   return (
     <>
-      <Suspense fallback={<LoadingPage />}>
+      
         <div className="w-full mx-auto overflow-hidden bg-[#111]/90 relative">
           <Hero />
         </div>
-      </Suspense>
+      <CookieBanner />
     </>
   );
 }
