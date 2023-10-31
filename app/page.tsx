136894aaa6e1d436454d84466ac6de8a79f10d80
Deaa Aldin Alawad <108593938+deaamanager@users.noreply.github.com>
2023-10-31 22:58:18 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,15 @@
 import CookieBanner from "@/components/Cookiesbanner";
 import Hero from "@/components/Hero";
-<<<<<<< HEAD
-=======
-import LoadingPage from "@/components/LoadingPage";
+
 import CookieBanner from "@/components/Cookiesbanner";
->>>>>>> 6a35b301af519cf231a5a0f66c993cda05f33101
+
 
 export default function Home() {
   return (
     <>
-<<<<<<< HEAD
       <div className="w-full mx-auto overflow-hidden bg-[#111]/90 relative">
         <Hero />
       </div>
-=======
-      
-        <div className="w-full mx-auto overflow-hidden bg-[#111]/90 relative">
-          <Hero />
-        </div>
->>>>>>> 6a35b301af519cf231a5a0f66c993cda05f33101
       <CookieBanner />
     </>
   );
